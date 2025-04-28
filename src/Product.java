68285c20c3ca652c0ddbc8050ca423cdd54c5916
CallumLee22 <callum.lee@stfc.ac.uk>
2025-04-28 11:04:04 +0100

    Add get id method for product

--- src/Product.java
+++ src/Product.java
@@ -3,9 +3,14 @@ public class Product {
     private String name;
     private int quantityInStock;
 
-    public Product(String name, int quantityInStock) {
+    public Product(int id, String name, int quantityInStock) {
+        this.id = id;
         this.name = name;
         this.quantityInStock = quantityInStock;
+    }
+
+    public int getId() {
+        return this.id;
     }
 
     public String getName() {
