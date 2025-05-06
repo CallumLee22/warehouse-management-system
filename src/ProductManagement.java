08bc42c64f01791c57cfda4c3bac24db5aa236c4
CallumLee22 <callum.lee@stfc.ac.uk>
2025-05-06 11:32:38 +0100

    Add products functionality

---
+++ src/ProductManagement.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class ProductManagement {
+    HashMap<Integer, Product> products = new HashMap<>();
+    private int nextId = 1;
+
+    public ProductManagement() {
+
+    }
+
+    public void addProduct(String name, double sellPrice, double buyPrice, int initialStock) {
+        products.put(nextId, new Product(name, sellPrice, buyPrice, initialStock));
+        nextId++;
+    }
+
+    public HashMap<Integer, Product> getProducts() {
+        return products;
+    }
+}
