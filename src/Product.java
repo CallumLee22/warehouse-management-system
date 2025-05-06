a89df647d296e9d047384b766e8522db3321aa97
CallumLee22 <callum.lee@stfc.ac.uk>
2025-05-06 10:34:54 +0100

    Add product class

--- src/Product.java
+++ src/Product.java
@@ -1,12 +1,16 @@
 public class Product {
-    private int id;
+    private final int id;
     private String name;
     private int quantityInStock;
+    private double sellPrice;
+    private double buyPrice;
 
-    public Product(int id, String name, int quantityInStock) {
+    public Product(int id, String name, int quantityInStock, double sellPrice, double buyPrice) {
         this.id = id;
         this.name = name;
         this.quantityInStock = quantityInStock;
+        this.sellPrice = sellPrice;
+        this.buyPrice = buyPrice;
     }
 
     public int getId() {
@@ -17,7 +21,31 @@ public class Product {
         return this.name;
     }
 
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
     public int getQuantityInStock() {
         return this.quantityInStock;
+    }
+
+    public void setQuantityInStock(int newQuantity) {
+        this.quantityInStock = newQuantity;
+    }
+
+    public double getSellPrice() {
+        return this.sellPrice;
+    }
+
+    public void setSellPrice(double newSellPrice) {
+        this.sellPrice = newSellPrice;
+    }
+
+    public double getBuyPrice() {
+        return this.buyPrice;
+    }
+
+    public void setBuyPrice(double newBuyPrice) {
+        this.buyPrice = newBuyPrice;
     }
 }
