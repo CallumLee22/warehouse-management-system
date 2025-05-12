d35f2e33cf032a7c9cf7949f0f06cbb6340cc362
CallumLee22 <callum.lee@stfc.ac.uk>
2025-05-12 16:01:29 +0100

    Allow orders to be sent and update stock

--- src/SellOrderManagement.java
+++ src/SellOrderManagement.java
@@ -10,4 +10,17 @@ public class SellOrderManagement extends OrderManagement<SellOrder> {
     protected SellOrder createOrderInstance(int orderId, ArrayList<OrderProductEntry> products) {
         return new SellOrder(orderId, products);
     }
+
+    @Override
+    public void createOrder(ArrayList<OrderProductEntry> products) {
+        SellOrder order = createOrderInstance(nextOrderId, products);
+        orders.put(nextOrderId, order);
+        nextOrderId++;
+
+        for (OrderProductEntry entry : products) {
+            int productId = entry.product().getId();
+            int quantity = entry.quantity();
+            updateStock(productId, -quantity);
+        }
+    }
 }
